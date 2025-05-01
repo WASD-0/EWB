90cdd170b79f42b70b3a7b7373db2eb094a3d212
WASD-0 <104704943+WASD-0@users.noreply.github.com>
2025-05-01 17:31:21 +1000

    Update and rename Lora.cpp to LoraSender.cpp

--- LoraSender.cpp
+++ LoraSender.cpp
@@ -1,19 +1,21 @@
 #include <SPI.h>
 #include <LoRa.h>
-// file responible to send data 
+
 void setup() {
   Serial.begin(9600);
   LoRa.setPins(5, 14, 26); // NSS, RESET, DIO0
-  if (!LoRa.begin(433E6)) {
-    Serial.println("LoRa init failed");
+  if (!LoRa.begin(915E6)) {
+    Serial.println("LoRa init failed.");
     while (1);
   }
 }
 
 void loop() {
-  String data = "station1,wind=40,rain=3";
+  String data = "TO:BASE,FROM:ST1,wind=43,rain=2";
+
   LoRa.beginPacket();
   LoRa.print(data);
   LoRa.endPacket();
-  delay(10000); // send every 10s
+
+  delay(10000); // Send every 10s
 }
